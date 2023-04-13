b27a9684dcef789e370ee1f8b95fdd8c5d9bc085
Chau Nguyen <43297448+VanQuishi@users.noreply.github.com>
2023-04-13 08:13:14 -0500

    DOM through npm

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-console.log("Hello");
+const contentDiv = document.createElement('div');
+
+/* headline */
+const headline = document.createElement('h1'); 
+headline.textContent = "Taco Bell Menu";
+console.log(headline);
+
+const imgDiv = document.createElement('div');
+imgDiv.style = "width: 50px;";
+const imgTag = document.createElement('img');
+imgTag.src = "../src/taco-icon.svg";
+imgDiv.appendChild(imgTag);
+
+const descriptionDiv = document.createElement('div');
+const h2Tag = document.createElement('h2');
+h2Tag.textContent = "Find tacos near you today";
+descriptionDiv.appendChild(h2Tag);
+
+contentDiv.appendChild(headline);
+contentDiv.appendChild(imgDiv);
+contentDiv.appendChild(descriptionDiv);
+
+document.body.appendChild(contentDiv);
