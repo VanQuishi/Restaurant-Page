a5d0cbdb536fc2eb434e5c8652033d87f0f3d2e7
Chau Nguyen <43297448+VanQuishi@users.noreply.github.com>
2023-05-02 22:16:43 -0500

    fix main content display

--- src/contact.js
+++ src/contact.js
@@ -9,11 +9,12 @@ h1Title.textContent = "Contact";
 titleWrapper.appendChild(h1Title);
 
 const actionCallWrapper = document.createElement('div');
+actionCallWrapper.classList.add('moduleChild');
 let h2Title = document.createElement('h2');
 h2Title.textContent = "Let's keep the talk on Tacos going!";
 actionCallWrapper.appendChild(h2Title);
 
-const emailWrapper = document.createElement('div');
+/* const emailWrapper = document.createElement('div');
 let emailInput = document.createElement('input');
 emailInput.type = "email";
 emailInput.placeholder = "Your email";
@@ -30,13 +31,14 @@ const button = document.createElement('button')
 button.textContent = "Submit";
 
 const formWrapper = document.createElement('div');
+formWrapper.classList.add('moduleChild');
 let emailForm = document.createElement('form');
 emailForm.appendChild(emailWrapper);
 emailForm.appendChild(contentWrapper);
 formWrapper.appendChild(emailForm);
-formWrapper.appendChild(button);
+formWrapper.appendChild(button); */
 
 contact.appendChild(titleWrapper);
 contact.appendChild(actionCallWrapper);
-contact.appendChild(formWrapper);
+/* contact.appendChild(formWrapper); */
 export default contact;
