7b282d81d8e8b7eb6ce0d16932b901d617901e4a
Chau Nguyen <43297448+VanQuishi@users.noreply.github.com>
2023-05-04 22:09:26 -0500

    add source code link

--- src/pages/footer.js
+++ src/pages/footer.js
@@ -6,6 +6,15 @@ let creditLine = document.createElement('p');
 creditLine.textContent = "Made by Nin";
 creditWrapper.appendChild(creditLine);
 
+const sourceCodeWrapper = document.createElement('div');
+let link = document.createElement('a');
+link.href = "https://github.com/VanQuishi/Restaurant-Page";
+link.textContent = "View source code on Github";
+link.target = "_blank";
+link.style.textDecoration = "none";
+sourceCodeWrapper.appendChild(link);
+
 footer.appendChild(creditWrapper);
+footer.appendChild(sourceCodeWrapper);
 
 export default footer;
